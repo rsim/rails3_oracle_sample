9ac814527656f75ceecae7d998c00bf2597c6fb6
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2010-09-09 17:51:39 +0300

    updated oracle initializer according to latest oracle_enhanced adapter changes

--- config/initializers/oracle.rb
+++ config/initializers/oracle.rb
@@ -2,11 +2,7 @@
 # will be the same as Ruby Time.local time zone
 ENV['TZ'] = 'Europe/Riga'
 
-require "active_record/connection_adapters/oracle_enhanced_tasks"
-
 ActiveSupport.on_load(:active_record) do
-
-  require "active_record/connection_adapters/oracle_enhanced_adapter"
 
   ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter.class_eval do
     # some sample settings that I use in my projects
@@ -22,18 +18,21 @@ ActiveSupport.on_load(:active_record) do
 
     # to ensure that sequences will start from 1 and without gaps
     self.default_sequence_start_value = "1 NOCACHE INCREMENT BY 1"
+
+    # Cache column descriptions between requests.
+    # Highly recommended as currently Arel is doing a lot of additional queries
+    # to get table columns and primary key.
+    # If this is used then you need to restart server in development environment
+    # after running migrations which change table columns.
+    # By default caching is enabled just in test and production environments.
+    if ['development', 'test', 'production'].include? Rails.env
+      self.cache_columns = true
+    end
+
   end
 
   # PL/SQL connection
   plsql.activerecord_class = ActiveRecord::Base
 
-  # Cache column descriptions between requests.
-  # Highly recommended as currently Arel is doing a lot of additional queries
-  # to get table columns and primary key.
-  # If this is used then you need to restart server in development environment
-  # after running migrations which change table columns.
-  if ['development', 'test', 'production'].include? Rails.env
-    ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter.cache_columns = true
-  end
 
 end
